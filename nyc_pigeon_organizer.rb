2e700bd6ff9b76d04113d0afa35ea0d6ad53099d
David Goodman <davegoodman20@gmail.com>
2017-08-24 19:52:04 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,11 @@
-require 'pry'
 def nyc_pigeon_organizer(data)
-  data.each do |k, v|
-    puts k
-    binding.pry
+  data.each_with_object({}) do |(key, h), result|
+    h.each do |value, names|
+      names.each do |name|
+        result[name] ||= {}
+        result[name][key] ||= []
+        result[name][key] << value.to_s
+      end
+    end
   end
-  binding.pry
 end
